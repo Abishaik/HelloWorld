7268ec079d1f17bde75ed526c2f4bd1d6a5db454
Abishaik <69312667+Abishaik@users.noreply.github.com>
2021-01-08 18:15:02 +0530

    R
    R

---
+++ r.r
@@ -0,0 +1,13 @@
+# We can use the print() function
+print("Abishaik !")
+#Output:[1] "Abishaik !"
+
+
+# Quotes can be suppressed in the output
+print("Abishaik !", quote = FALSE)
+#Output:[1] Abishaik !
+
+
+# If there are more than 1 item, we can concatenate using paste()
+print(paste("How","are","you?"))
+#Output:[1] "How are you?"
